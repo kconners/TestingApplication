5b9aacd21aa370ed724179d2b3b19a832e9abe2a
kconners <kconners@brierley.com>
2018-05-17 16:23:39 -0500

    Fixed Most of the Index page.

--- TestingAppNew/Script/Product.js
+++ TestingAppNew/Script/Product.js
@@ -7,7 +7,7 @@
         $.each(obj, function (idx, item) {
             
             $.each(item, function (i, ii) {
-                if (version_idnumber == "0") {
+                if (version_idnumber === "0") {
                     version_idnumber = ii.version_idnumber;
                     console.log(version_idnumber);
 
@@ -18,7 +18,7 @@
             
         });
                 
-            
+        loadLWBuild();
         });
 
     //$('select option:first-child').attr("selected", "selected");
@@ -52,6 +52,77 @@ function loadLWBuild() {
         $.each(obj, function (idx, item) {
             $.each(item, function (i, ii) {
                 dropdown.append($('<option></option>').attr('value', ii.build_idnumber).text(ii.build_name));
+            });
+
+        });
+
+        if ($('#Select_Build option').length == 1) {
+
+            loadVersionBuildInfo();
+        }
+    });
+    dropdown.prop('selectedIndex', 1);
+}
+function loadVersionBuildInfo() {
+   
+    var _versionIDNUM = $('#Select_LWVersion').val();
+    var _buildIDNUM = $('#Select_Build').val();
+
+    $.getJSON(qadataendPoint + "/VersionBuildDetails?version_idnum=" + _versionIDNUM + "&build_idnum=" + _buildIDNUM, function (obj) {
+        $.each(obj, function (idx, item) {
+            $.each(item, function (i, ii) {
+                if (i === "url") {
+                    $("#link_loyaltynavigator").attr("href", ii);
+                    $("#link_loyaltynavigator").text(ii);
+                }
+                if (i === "schema") {
+                    $("#label_navigatorshema").text(ii);
+                }
+            });
+
+        });
+
+
+    });
+
+    loadOrgs();
+
+   
+}
+
+
+
+function loadOrgs() {
+    var _version = $('#Select_LWVersion option:selected').text();
+    var _build = $('#Select_Build option:selected').text();
+    let dropdown = $('#Select_org');
+    dropdown.empty();
+    $.getJSON(qadataendPoint + "/TestClient?release=" + _version + "&build=" + _build, function (obj) {
+        $.each(obj, function (idx, item) {
+
+            $.each(item, function (i, ii) {
+                dropdown.append($('<option></option>').attr('value', ii.client_idnum).text(ii.client_name));
+            });
+        });
+
+        if ($('#Select_org option').length == 1) {
+            
+            loadOrgsEnv();
+        }
+    });
+    
+}
+
+function loadOrgsEnv() {
+    var org_IDNUM = $('#Select_org').val();
+    let dropdown = $('#Select_orgenv');
+    dropdown.empty();
+
+
+    $.getJSON(qadataendPoint + "/TestClientEnv?Org_IDNUM=" + org_IDNUM, function (obj) {
+        $.each(obj, function (idx, item) {
+            $.each(item, function (i, ii) {
+                dropdown.append($('<option></option>').attr('value', ii.Env_IDNumber).text(ii.Environment));
             });
 
         });
