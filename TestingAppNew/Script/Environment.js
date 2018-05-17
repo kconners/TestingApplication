5d87a9d7f081f6297756250aecfc50398adb4800
kconners <kconners@brierley.com>
2018-05-17 14:19:26 -0500

    Fixed LW Client Dropdown

---
+++ TestingAppNew/Script/Environment.js
@@ -0,0 +1,1 @@
+﻿var qadataendPoint = "http://cy2qawbs06.brierley.com:8115/api";
--- TestingAppNew/Script/Product.js
+++ TestingAppNew/Script/Product.js
@@ -1,9 +1,62 @@
 ﻿function loadLWVersion() {
-    console.log("hELLO");
-    $.getJSON("http://cy2qawbs06.brierley.com:8115/api/LoyaltyVersion", function (obj) {
-        $.each(obj, function (key, value) {
-            $("select").append($('<option>', { value: obj.version_idnumber, text: obj.version_name }))
-            console.log(obj.version_name);
+    var version_idnumber = "0"
+
+    let dropdown = $('#Select_LWVersion');
+    dropdown.empty();
+    $.getJSON(qadataendPoint+"/LoyaltyVersion", function (obj) {
+        $.each(obj, function (idx, item) {
+            
+            $.each(item, function (i, ii) {
+                if (version_idnumber == "0") {
+                    version_idnumber = ii.version_idnumber;
+                    console.log(version_idnumber);
+
+                    loadLWBuild(version_idnumber);
+                }
+                dropdown.append($('<option></option>').attr('value', ii.version_idnumber).text(ii.version_name));
+            });
+            
         });
+                
+            
+        });
+
+    //$('select option:first-child').attr("selected", "selected");
+
+   
+}
+function loadLWBuild(Version_IDNUM) {
+    let dropdown = $('#Select_Build');
+    dropdown.empty();
+    dropdown.prop('selectedIndex', 0);
+
+    $.getJSON(qadataendPoint + "/LoyaltyBuild?version_idnum=" + Version_IDNUM, function (obj) {
+        $.each(obj, function (idx, item) {
+            $.each(item, function (i, ii) {
+                dropdown.append($('<option></option>').attr('value', ii.build_idnumber).text(ii.build_name));
+            });
+
+        });
+
+
     });
+    dropdown.prop('selectedIndex', 1);
+}
+function loadLWBuild() {
+    var Version_IDNUM = $('#Select_LWVersion').val();
+    let dropdown = $('#Select_Build');
+    dropdown.empty();
+    dropdown.prop('selectedIndex', 0);
+    
+    $.getJSON(qadataendPoint +"/LoyaltyBuild?version_idnum=" + Version_IDNUM, function (obj) {
+        $.each(obj, function (idx, item) {
+            $.each(item, function (i, ii) {
+                dropdown.append($('<option></option>').attr('value', ii.build_idnumber).text(ii.build_name));
+            });
+
+        });
+
+
+    });
+    dropdown.prop('selectedIndex', 1);
 }
